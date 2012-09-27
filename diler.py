71b95e4ed5697d00b9f23bd4549e28a71d7ac165
ORuzhytskyi <oleksru.git@gmail.com>
2012-09-27 19:23:12 +0300

    Implemented pard of Diler methods

--- diler.py
+++ diler.py
@@ -1,12 +1,19 @@
 class Diler(object):
-	def __init__(self):
-		pass
+	def __init__(self, deck):
+		self.deck = deck	
 
 	def give_card(self):
-		pass
+		"""Returns Card object taken from a Deck"""
+		return deck.pop_card() 
 	
 	def give_two_cards(self):
-		pass
+		"""Returns a list of two Card objects"""
+		return [deck.pop_card() for i in range(2)]
 		
 	def give_three_cards(self):
+		"""Returns a list of three Card objects"""
+		return [deck.pop_card() for i in range(3)]
+	
+	def verify_move(self, move, history):
+		"""Verifies player's move according to given game history. Returns 0 if move is correct and 1 otherwise.""" 
 		pass
